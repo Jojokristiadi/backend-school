3b73849950fce718903823a07b27cd6b830f9b65
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-03-03 17:01:50 +0700

    Add greeting

---
+++ src/main/java/com/school/smp1/Greeting.java
@@ -0,0 +1,25 @@
+package com.school.smp1;
+
+public class Greeting {
+	private final long id;
+	private final String content;
+    private final String title;
+
+    public Greeting(long id, String content, String title) {
+        this.id = id;
+        this.content = content;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
