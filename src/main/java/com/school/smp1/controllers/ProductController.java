790153d276baf8f8269ae07cc8d82773f59d91b0
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-03-04 00:44:57 +0700

    Update product controller

--- src/main/java/com/school/smp1/controllers/ProductController.java
+++ src/main/java/com/school/smp1/controllers/ProductController.java
@@ -1,17 +1,27 @@
 package com.school.smp1.controllers;
 
+import java.util.List;
+
 import com.school.smp1.models.Product;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-
     @GetMapping("/products")
-    public Product product() {
+    public List<Product> fetch() {
         Product bando = new Product(1, "Bando", "Bagus dan indah", 2500);
-        return bando;
+        Product ikat = new Product(2, "Ikat Rambut", "Tebal dan lentur", 500);
+        return List.of(bando, ikat);
+    }
+    
+    @GetMapping("/products/{id}")
+    public Product fetchById(@PathVariable long id) {
+        if(id == 1) {
+            return new Product(1, "Bando", "Bagus dan indah", 2500);
+        }
+        return new Product(2, "Ikat Rambut", "Tebal dan lentur", 500);
     }
 }
