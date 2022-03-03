1e38abeaad5df91708bb60f8806c2be0a3870ff8
Joshua Kristiadi <joshua.kristiadi@student.umn.ac.id>
2022-03-03 21:28:02 +0700

    Greeting with request param

--- src/main/java/com/school/smp1/controllers/GreetingController.java
+++ src/main/java/com/school/smp1/controllers/GreetingController.java
@@ -3,14 +3,16 @@ package com.school.smp1.controllers;
 import com.school.smp1.models.Greeting;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public Greeting greeting() {
-        Greeting hello = new Greeting(1, "Hello World All");
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String custom) {
+        Greeting hello = new Greeting(1, "Hello " + custom);
         return hello;
     }
+    
 }
