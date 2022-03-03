2eb6a33345cc19fa08dd357895bacb4c9e60f07e
Joshua Kristiadi <joshua.kristiadi@student.umn.ac.id>
2022-03-03 21:17:07 +0700

    change Greeting Controller

--- src/main/java/com/school/smp1/controllers/GreetingController.java
+++ src/main/java/com/school/smp1/controllers/GreetingController.java
@@ -1,7 +1,16 @@
 package com.school.smp1.controllers;
 
+import com.school.smp1.models.Greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
+    @GetMapping("/greeting")
+    public Greeting greeting() {
+        Greeting hello = new Greeting(1, "Hello World");
+        return hello;
+    }
 }
