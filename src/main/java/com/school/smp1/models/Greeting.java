092a791b9dc9dfb7535806be6ce6a056443e6a6a
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-03-03 20:32:12 +0700

    Add greeting controller

--- src/main/java/com/school/smp1/models/Greeting.java
+++ src/main/java/com/school/smp1/models/Greeting.java
@@ -3,16 +3,10 @@ package com.school.smp1.models;
 public class Greeting {
 	private final long id;
 	private final String content;
-    private final String title;
 
-    public Greeting(long id, String content, String title) {
+    public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
-        this.title = title;
-    }
-
-    public String getTitle() {
-        return title;
     }
 
     public long getId() {
