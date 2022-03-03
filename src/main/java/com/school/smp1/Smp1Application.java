68b4dcf6dcff3450ae6328e57010b3d92c597287
Joshua Kristiadi <joshua.kristiadi@student.umn.ac.id>
2022-03-03 16:34:01 +0700

    initial commit

---
+++ src/main/java/com/school/smp1/Smp1Application.java
@@ -0,0 +1,13 @@
+package com.school.smp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Smp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Smp1Application.class, args);
+	}
+
+}
